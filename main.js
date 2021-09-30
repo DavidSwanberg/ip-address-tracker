ae07c82a808ed5ff2aa000a50da20d0eb2fe8f34
David Swanberg <DavidSwanberg@users.noreply.github.com>
2021-09-30 14:33:24 -0400

    [add] setInfo to js and fix map issues

--- main.js
+++ main.js
@@ -1,26 +1,58 @@
 import './style.css'
 
-let ip = "8.8.8.8";
-const api_key = "at_Sshnj6Ixj0r4TjUCpwRxrRkqoBBDi";
-const api_url = "https://geo.ipify.org/api/v1?";
-const url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip
-fetch(url).then(function (response) {
-  console.log ("response", response);
-}).catch(function (err) {
-  console.warn('error', err);
-});
+const mymap = L.map('mapid')
 
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const setMap =(data)=>{
+   mymap.setView([data.location.lat, data.location.lng], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZGF2aWRzd2FuYmVyZyIsImEiOiJja3RkNXJtZ3YyY2M5MndxbjIxanVtOGl2In0.O7NFEkwmWF2gnTPcxvEcqw'
-}).addTo(mymap);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiZGF2aWRzd2FuYmVyZyIsImEiOiJja3RkNXJtZ3YyY2M5MndxbjIxanVtOGl2In0.O7NFEkwmWF2gnTPcxvEcqw'
+  }).addTo(mymap);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+  L.marker([data.location.lat, data.location.lng]).addTo(mymap);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+}
+
+const setInfo =(info)=> {
+  document.getElementById("ip-address").textContent=`${info.ip}`;
+  document.getElementById("location").textContent=`${info.location.city}, ${info.location.region} ${info.location.postalCode}`;
+  document.getElementById("timezone").textContent=`${info.location.timezone}`;
+  document.getElementById("isp").textContent=`${info.isp}`;
+}
+
+const getData =(ip)=>{
+  const api_key = "at_Sshnj6Ixj0r4TjUCpwRxrRkqoBBDi";
+  const api_url = "https://geo.ipify.org/api/v1?";
+  const url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip
+  fetch(url)
+    .then((response)=> {
+      return response.json();
+    })
+    .then((json)=>{
+      console.log("json", json);
+      setInfo(json)
+      setMap(json)
+    })
+    .catch(function (err) {
+      console.warn('error', err);
+    });
+  };
+
+
+  const handleSubmit =(event)=> {
+    event.preventDefault();
+    const input = document.getElementById('input').value;
+    console.log('input', input)
+    getData(input)
+  }
+
+
+  const form = document.getElementById('form');
+  form.addEventListener('submit', handleSubmit);
+
+  getData('');
